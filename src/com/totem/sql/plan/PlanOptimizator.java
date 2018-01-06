866d7c3b6b1b511867db401ad5a7aef436cf54b9
Yangff <yangff1@gmail.com>
2018-01-07 02:53:36 +0800

    plan optimizator

--- src/com/totem/sql/plan/PlanOptimizator.java
+++ src/com/totem/sql/plan/PlanOptimizator.java
@@ -1,4 +1,11 @@
 package com.totem.sql.plan;
 
+/**
+ * Run optimization on original plan
+ *
+ * for example, `where a > 10 and a < 60` would generate Intersect(RangeScan(a, 10, inf) and RangeScan(a, inf, 60))
+ * We can change it to RangeScan(a, 10, 60)
+ *
+ */
 public class PlanOptimizator {
 }
