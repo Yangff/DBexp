6ee4149e7ce221c526ca34ea3cc5f14c76b2c94e
Yangff <yangff1@gmail.com>
2018-01-07 02:54:02 +0800

    add combinator

--- src/com/totem/engine/iteratorCombinator/IntersectCombinator.java
+++ src/com/totem/engine/iteratorCombinator/IntersectCombinator.java
@@ -5,13 +5,13 @@ import com.totem.table.Cell;
 import java.util.Iterator;
 
 public class IntersectCombinator implements Iterator<Cell[]> {
-    @Override
-    public boolean hasNext() {
+@Override
+public boolean hasNext() {
         return false;
-    }
+        }
 
-    @Override
-    public Cell[] next() {
+@Override
+public Cell[] next() {
         return new Cell[0];
-    }
-}
+        }
+        }
