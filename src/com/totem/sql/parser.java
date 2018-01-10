5ebae36e9b621e0202bb9307c4a8e9c88e10f3da
Yangff <yangff1@gmail.com>
2018-01-10 15:19:00 +0800

    add comment

--- src/com/totem/sql/parser.java
+++ src/com/totem/sql/parser.java
@@ -1,10 +1,22 @@
 package com.totem.sql;
 
 import com.totem.database;
+import com.totem.sql.action.Expression;
 
 public class parser {
     private database db;
     public parser(database db){
         this.db = db;
     }
+
+    /**
+     * parse {@code sql} can generate action
+     * used for parse user input and switch expression of virtual attributes
+     * @param sql sql input, maybe expression
+     * @return Action, one of the followings: CreateIndex, CreateTable, Insert, Select, Expression
+     */
+    public Expression Parse(String sql){
+        return null;
+    }
+
 }
